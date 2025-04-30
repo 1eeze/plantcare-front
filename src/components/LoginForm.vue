0c096b3c3f7183e4b7bedae7951f667e58f321d9
1eeze <leezeze01@naver.com>
2025-04-30 19:28:03 +0900

    카카오 / 네이버 계정 로그인 버튼 추가

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -29,12 +29,38 @@
         </div>
       <div class="sns-circle-group">
 
+        <!-- 네이버 로그인 버튼 -->
+        <div class="sns-circle-button" @click="handleNaverLogin('naver')">
+          <button class="gsi-material-button">
+            <div class="gsi-material-button-state"></div>
+            <div class="gsi-material-button-content-wrapper">
+              <div class="gsi-material-button-icon">
+                <img class="naver-img" src="../assets/btnW_아이콘원형.png" alt="네이버" />
+              </div>
+            </div>
+          </button>
+          <span>네이버</span>
+        </div>
+
+        <!-- 카카오오 로그인 버튼 -->
+        <div class="sns-circle-button" @click="handleKakaoLogin('naver')">
+          <button class="gsi-material-button">
+            <div class="gsi-material-button-state"></div>
+            <div class="gsi-material-button-content-wrapper">
+              <div class="gsi-material-button-icon">
+                <img class="kakao-img" src="../assets/kakao-talk.png" alt="카카오오" />
+              </div>
+            </div>
+          </button>
+          <span>카카오</span>
+        </div>
+
         <!-- 구글 로그인 -->
         <div class="sns-circle-button" @click="handleGoogleLogin('google')">
           <button class="gsi-material-button">
             <div class="gsi-material-button-state"></div>
             <div class="gsi-material-button-content-wrapper">
-              <div class="gsi-material-button-icon">
+              <div class="gsi-material-button-icon" id="google-icon-wrapper">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" style="display: block;">
                   <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
                   <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
@@ -61,6 +87,14 @@
   const userPassword = ref('')
   const secureLogin = ref(false)
   const router = useRouter()
+
+  const handleNaverLogin = () => {
+    window.location.href = 'https://pkplantcare.shop/auth/login/naver'
+  }
+
+  const handleKakaoLogin = () => {
+    window.location.href = 'https://pkplantcare.shop/auth/login/kakao'
+  }
 
   const handleGoogleLogin = () => {
   window.location.href = 'https://pkplantcare.shop/login'
@@ -141,14 +175,14 @@ button {
 }
 
 .login-button {
-  background-color: #02c966;
+  background-color: #568265;
   color: white;
 }
 
 .signup-button {
   background-color: white;
-  border: 1px solid #02c966;
-  color: #02c966;
+  border: 1px solid #568265;
+  color: #568265;
 }
 
 .sns-divider {
@@ -203,6 +237,7 @@ button {
   border-radius: 50%;
   width: 50px;
   height: 50px;
+  padding: 0;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -215,14 +250,45 @@ button {
 }
 
 .gsi-material-button .gsi-material-button-icon {
-  width: 24px;
-  height: 24px;
+  width: 100%;
+  height: 100%;
   padding: 0;
   margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .gsi-material-button svg {
   width: 100%;
   height: 100%;
 }
+
+.naver-img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  border-radius: 50%;
+}
+
+.kakao-img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  border-radius: 50%;
+}
+
+#google-icon-wrapper {
+  width: 100%;
+  height: 100%;
+  padding: 11px;
+  box-sizing: border-box;
+}
+
+#google-icon-wrapper svg {
+  width: 90%;
+  height: 90%;
+  object-fit: contain;
+}
+
 </style>
